379f21ef44f263752e40be956496e337b8ba8c05
James <james.ide775@gmail.com>
2023-12-10 08:24:04 +1030

    refactor file upload

--- client/src/app/shared/file-upload/file-upload.component.ts
+++ client/src/app/shared/file-upload/file-upload.component.ts
@@ -13,6 +13,10 @@ import { DragDropDirective } from '../../core/directives/drag-drop.directive';
 })
 export class FileUploadComponent {
   showUploadDiv: boolean = false;
+  allowedFileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  maxFileSize = 1000000;
+
+  FILE_ERROR_MESSAGE: string = '';
 
   uploading: boolean = false;
 
@@ -31,12 +35,28 @@ export class FileUploadComponent {
   }
 
   onFileChange(ev: Event | File) {
-    let file: File | undefined;
-    if (ev instanceof Event) {
-      file = (ev.target as HTMLInputElement).files![0];
-    } else if (ev instanceof File) {
-      file = ev;
+    const file = this.extractFile(ev);
+    console.log(file?.size);
+    if (file) {
+      if (file.size > this.maxFileSize) {
+        this.FILE_ERROR_MESSAGE = 'File size cannot exceed 1MB';
+        return;
+      } else if (!this.allowedFileTypes.includes(file.type)) {
+        this.FILE_ERROR_MESSAGE = 'File must be a jpeg or png';
+        return;
+      } else {
+        this.fileSelected.emit(file);
+      }
     }
-    this.fileSelected.emit(file);
+  }
+
+  extractFile(ev: Event | File): File | undefined {
+    if (ev instanceof Event) {
+      return (ev.target as HTMLInputElement).files?.[0];
+    } else if (ev instanceof File) {
+      return ev;
+    }
+
+    return undefined;
   }
 }
